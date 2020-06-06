4085b8837354fedc168b334df08019a1eb996045
Pham Hoang Anh <59822643+hoanganh8499hd@users.noreply.github.com>
2020-06-06 23:27:06 +0700

    change url

--- thuc-tap-nhom/App_Start/RouteConfig.cs
+++ thuc-tap-nhom/App_Start/RouteConfig.cs
@@ -18,7 +18,7 @@ namespace thuc_tap_nhom
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "index", id = UrlParameter.Optional }
             );
         }
     }
