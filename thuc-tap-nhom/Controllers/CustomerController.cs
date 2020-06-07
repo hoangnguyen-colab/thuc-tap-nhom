6b421004aa270e373857b2a4787f1971cadb5eea
Pham Hoang Anh <59822643+hoanganh8499hd@users.noreply.github.com>
2020-06-07 15:52:50 +0700

    add controller

---
+++ thuc-tap-nhom/Controllers/CustomerController.cs
@@ -0,0 +1,96 @@
+﻿using Models.DAO;
+using Models.EF;
+using cong_nghe_web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Data.SqlClient;
+
+namespace cong_nghe_web.Controllers
+{
+    public class CustomerController : Controller
+    {
+        [Route("login")]
+        public ActionResult Login()
+        {
+            var name = HttpContext.User.Identity.Name;
+            if (!string.IsNullOrEmpty(name))
+            {
+                return RedirectToAction("CustomerProfile", "Customer", new { username = name });
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult ValidateUser(LoginModel model, bool RememberMe)
+        {
+            /*<membership defaultProvider="CustomMembershipProvider">
+          <providers>
+            <add name="CustomMembershipProvider" connectionStringName="ShopDienThoaiDbContext" type="cong_nghe_web.Common.CustomMembershipProvider" />
+          </providers>
+        </membership>
+            if (Membership.ValidateUser(model.CustomerUsername, model.CustomerPassword) && ModelState.IsValid)
+        */
+            if (new CustomerDAO().Login(model.CustomerUsername, model.CustomerPassword) && ModelState.IsValid)
+            {
+                FormsAuthentication.SetAuthCookie(model.CustomerUsername, RememberMe);
+                return Json(new { Success = true, Username = model.CustomerUsername }, JsonRequestBehavior.AllowGet);
+            }
+            else
+                return Json(new { Success = false }, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public async Task<JsonResult> RegisterCustomer(RegisterCustomer model)
+        {
+            var dao = new CustomerDAO();
+            if (!await dao.CheckUser(model.CustomerUsername))
+            {
+                try
+                {
+                    int result = await new CustomerDAO().Register(new CUSTOMER()
+                    {
+                        CustomerUsername = model.CustomerUsername,
+                        CustomerPassword = model.CustomerPassword,
+                        CustomerName = model.CustomerName,
+                        CustomerPhone = model.CustomerPhone,
+                        CustomerEmail = model.CustomerEmail,
+                        CreatedDate = DateTime.Now
+                    });
+                    return Json(new { ReturnID = 1 }, JsonRequestBehavior.AllowGet);
+                }
+                catch
+                {
+                    return Json(new { ReturnID = 2 }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+                return Json(new { ReturnID = 0 }, JsonRequestBehavior.AllowGet);
+        }
+
+        [Authorize]
+        [ActionName("Logout")]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Customer");
+        }
+
+        [Authorize]
+        [Route("profile/{username}")]
+        public async Task<ActionResult> CustomerProfile(string username)
+        {
+            var membername = HttpContext.User.Identity.Name;
+            if (!membername.Equals(username))
+            {
+                return View(await new CustomerDAO().LoadByUsername(membername));
+            }
+            return View(await new CustomerDAO().LoadByUsername(username));
+        }
+    }
+}
---
+++ thuc-tap-nhom/Controllers/OrderController.cs
@@ -0,0 +1,57 @@
+﻿using cong_nghe_web.Models;
+using Models.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cong_nghe_webWeb.Controllers
+{
+    public class OrderController : Controller
+    {
+        [HttpPost]
+        [Authorize]
+        public async Task<JsonResult> CancelOrder(int OrderID)
+        {
+            //5 - cancel
+            var result = await new OrderDAO().ChangeOrder(OrderID, 5);
+            if (result == 0)
+            {
+                return Json(new { Success = false }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { Success = true }, JsonRequestBehavior.AllowGet);
+        }
+
+        [Authorize]
+        public async Task<ActionResult> OrderList(int? StatusID)
+        {
+            var membername = HttpContext.User.Identity.Name;
+            var customer = await new CustomerDAO().LoadByUsername(membername);
+
+            //var list = await new OrderDAO().LoadOrder<OrderModel>(customer.CustomerID);
+
+            var list = await new OrderDAO().LoadOrder(customer.CustomerID);
+
+            if (!StatusID.Equals(0))
+            {
+                list = list.Where(x => x.OrderStatusID == StatusID).ToList();
+            }
+
+            return PartialView("OrderList", list);
+        }
+
+        [Authorize]
+        public async Task<ActionResult> OrderProductList(int OrderID)
+        {
+            return PartialView("OrderProductList", await new OrderDAO().LoadProductOrder(OrderID));
+        }
+
+        public async Task<JsonResult> GetStatus()
+        {
+            var status = await new OrderStatusDAO().LoadStatus();
+            return Json(status, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
