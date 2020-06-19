5cbd9e02ffd1651195979acafa1e1c7d98c42643
Pham Hoang Anh <59822643+hoanganh8499hd@users.noreply.github.com>
2020-06-19 11:06:58 +0700

    add admin page

---
+++ DataAccess/DAO/CategoryDAO.cs
@@ -0,0 +1,87 @@
+﻿using DataAccess.EF;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAO
+{
+    public class CategoryDAO
+    {
+        EShopDbContext db = null;
+        public CategoryDAO()
+        {
+            db = new EShopDbContext();
+            db.Configuration.ProxyCreationEnabled = false;
+        }
+
+        public async Task<List<Category>> LoadData()
+        {
+            return await db.Categories.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Category> LoadByID(int id)
+        {
+            return await db.Categories.FindAsync(id);
+        }
+
+        public async Task<Category> LoadByURL(string url)
+        {
+            return await db.Categories
+                .AsNoTracking()
+                .Where(x => x.CategoryURL.Equals(url))
+                .SingleOrDefaultAsync();
+        }
+
+        public async Task<int> CreateBrand(Category cate)
+        {
+            try
+            {
+                db.Categories.Add(cate);
+                await db.SaveChangesAsync();
+                return cate.CategoryID;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public async Task<bool> DeleteBrand(int ID)
+        {
+            try
+            {
+                var prod = await db.Categories.Where(x => x.CategoryID == ID).SingleOrDefaultAsync();
+                db.Categories.Remove(prod);
+                await db.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<int> EditBrand(Category cate, int ID)
+        {
+            try
+            {
+                var item = db.Categories.Find(ID);
+                if (item == null)
+                {
+                    return 0;
+                }
+                item.CategoryName = cate.CategoryName;
+                item.CategoryURL= cate.CategoryURL;
+                await db.SaveChangesAsync();
+                return item.CategoryID;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ thuc-tap-nhom/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,75 @@
+﻿using Models.DAO;
+using Models.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cong_nghe_web.Areas.Admin.Controllers
+{
+    [RouteArea("Admin")]
+    public class BrandController : BaseController
+    {
+        public ActionResult CreateBrand()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateBrand(BRAND model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.BrandURL = SlugGenerator.SlugGenerator.GenerateSlug(model.BrandName);
+                model.CreatedDate = DateTime.Now;
+                int result = await new BrandDAO().CreateBrand(model);
+                return RedirectToAction("CreateBrand");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> EditBrand(BRAND model, int id)
+        {
+            if (ModelState.IsValid)
+            {
+                model.BrandURL = SlugGenerator.SlugGenerator.GenerateSlug(model.BrandName);
+                int result = await new BrandDAO().EditBrand(model, id);
+                if (result == 0)
+                {
+                    return Json(new { Success = false, id }, JsonRequestBehavior.AllowGet);
+                }
+                return Json(new { Success = true, id }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { Success = false, id }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> BrandList()
+        {
+            return PartialView("BrandList", await new BrandDAO().LoadData());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> BrandListPartial()
+        {
+            return PartialView("BrandListPartial", await new BrandDAO().LoadData());
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> DeleteBrand(int id)
+        {
+            if (await new BrandDAO().LoadByID(id) == null)
+            {
+                return Json(new { Success = 0 }, JsonRequestBehavior.AllowGet);
+            }
+            if (!(await new BrandDAO().DeleteBrand(id)))
+            {
+                return Json(new { Success = 0 }, JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { Success = 1 }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
